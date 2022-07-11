7f071f5c8a60636efcb6b2eb82508810ddf133a1
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-07-11 13:03:22 +0630

    Implementing Query Component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import github from './db.js';
+import query from './Query.js';
 const App = () => {
   const [userName, setUserName] = useState("")
   useEffect(() => {
-    const githubQuery = {
-      query: `
-      {
-        viewer {
-          name
-        }
-      } 
-      `,
-    };
+    
     fetch(github.baseURL, {
       method: "POST",
       headers: github.headers,
-      body: JSON.stringify(githubQuery),
+      body: JSON.stringify(query),
     })
     .then(response => response.json())
     .then(data => {
