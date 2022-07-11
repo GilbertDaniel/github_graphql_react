1196562e1b3907e71c0d7af692ee3c9ccc8d50ef
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-07-11 12:51:07 +0630

    Retrieving Data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import github from './db.js';
 const App = () => {
+  useEffect(() => {
+    const githubQuery = {
+      query: `
+      {
+        viewer {
+          name
+        }
+      } 
+      `,
+    };
+    fetch(github.baseURL, {
+      method: "POST",
+      headers: github.headers,
+      body: JSON.stringify(githubQuery),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
   return (
     <div className='container mt-5'>
       <h1 className='text-primary'>
