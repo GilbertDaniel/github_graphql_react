79966f165bbc048b563d096561a4927f9fbcffa4
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-07-11 14:54:20 +0630

    Implementing Callback Hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import github from './db.js';
 import query from './Query.js';
 const App = () => {
   const [userName, setUserName] = useState("")
-  useEffect(() => {
-    
+
+  const fetchData = useCallback(() => {
     fetch(github.baseURL, {
       method: "POST",
       headers: github.headers,
       body: JSON.stringify(query),
     })
-    .then(response => response.json())
-    .then(data => {
-      setUserName(data.data.viewer.name)
-      console.log(userName)
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  }, [])
+      .then(response => response.json())
+      .then(data => {
+        setUserName(data.data.viewer.name)
+        console.log(userName)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
   return (
     <div className='container mt-5'>
       <h1 className='text-primary'>
