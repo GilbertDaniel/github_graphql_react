d34241295d2c125c24755e08a3e5ab026cb56449
wintun <koko.wtun@gmail.com>
2022-07-13 15:50:38 +0630

    Implementing db example

---
+++ src/db.example.js
@@ -0,0 +1,11 @@
+
+const github = {
+    baseURL : "https://api.github.com/graphql",
+    username: "ADD_YOUR_GITHUB_USERNAME",
+    headers:{
+        "Content-Type": "application/json",
+        "Authorization": "bearer ADD_YOUR_PERSONAL_ACCESS_TOKEN_HERE"
+    }
+}
+
+export default github
