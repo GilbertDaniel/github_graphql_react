5b165c1e759be591cac06a6e0aa86a25364b19f6
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-08-29 21:39:40 +0630

    Updating React

--- src/index.js
+++ src/index.js
@@ -5,4 +5,11 @@ import App from './App';
 
 import './custom.scss'
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+// ReactDOM.render(<App/>, document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
